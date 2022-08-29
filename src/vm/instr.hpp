2a1c526325517478a8404fe5ffd04d83da9198fe
Andrew Orazov <orazov.ae@phystech.edu>
2022-08-29 16:09:27 +0300

    Test stack and initialize

--- src/vm/instr.hpp
+++ src/vm/instr.hpp
@@ -15,9 +15,9 @@ struct Instr {
   InstrType type;
 
   // Look at the specification in the interpretation
-  u_int8_t arg1;
-  u_int8_t arg2;
-  u_int8_t arg3;
+  u_int8_t arg1 = 0;
+  u_int8_t arg2 = 0;
+  u_int8_t arg3 = 0;
 };
 
 //////////////////////////////////////////////////////////////////////
--- tests/vm/cases.cpp
+++ tests/vm/cases.cpp
@@ -15,8 +15,36 @@ TEST_CASE("Return exit code", "[vm]") {
                         .arg1 = 0}                          //
           },                                                //
       //
-      .attached_vals{vm::rt::PrimitiveValue{.tag = vm::rt::ValueTag::Int,  //
-                                            .as_int = 123}},
+      .attached_vals{
+          vm::rt::PrimitiveValue{.tag = vm::rt::ValueTag::Int,  //
+                                 .as_int = 123},                //
+      },
+  };
+
+  CHECK(interpreter.Interpret(&chunk) == 123);
+}
+
+//////////////////////////////////////////////////////////////////////
+
+TEST_CASE("Push and pop", "[vm]") {
+  vm::BytecodeInterpreter interpreter;
+
+  vm::ExecutableChunk chunk{
+      .instructions =
+          {
+              vm::Instr{.type = vm::InstrType::PUSH_STACK,  //
+                        .arg1 = 0},                         //
+              vm::Instr{.type = vm::InstrType::PUSH_STACK,  //
+                        .arg1 = 1},                         //
+              vm::Instr{.type = vm::InstrType::POP_STACK},  //
+          },                                                //
+      //
+      .attached_vals{
+          vm::rt::PrimitiveValue{.tag = vm::rt::ValueTag::Int,   //
+                                 .as_int = 123},                 //
+          vm::rt::PrimitiveValue{.tag = vm::rt::ValueTag::Bool,  //
+                                 .as_bool = true},               //
+      },
   };
 
   CHECK(interpreter.Interpret(&chunk) == 123);
