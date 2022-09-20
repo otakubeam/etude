d2949c3474affc5cfc8b0c6dbb5264e1147a3289
Andrew Orazov <orazov.ae@phystech.edu>
2022-09-20 11:21:48 +0300

    Remove old interpreter runtime (rt directory)

--- tests/ast/environment.cpp
+++ tests/ast/environment.cpp
@@ -1,99 +1,96 @@
 #include <ast/scope/environment.hpp>
-#include <rt/base_object.hpp>
 
 // Finally,
 #include <catch2/catch.hpp>
 
-using namespace rt;
-
 //////////////////////////////////////////////////////////////////////
 
 TEST_CASE("Scoping", "[rt]") {
-  Environment<SBObject> global = Environment<SBObject>::MakeGlobal();
+  Environment<int> global = Environment<int>::MakeGlobal();
 
-  Environment<SBObject>* interpreter_scope = &global;
-  interpreter_scope->Declare("a", FromPrim(3));
-  CHECK(interpreter_scope->Get("a") == FromPrim(3));
+  Environment<int>* interpreter_scope = &global;
+  interpreter_scope->Declare("a", (3));
+  CHECK(interpreter_scope->Get("a") == (3));
 
   {
-    Environment<SBObject>::ScopeGuard guard1{&interpreter_scope};
+    Environment<int>::ScopeGuard guard1{&interpreter_scope};
 
     // Shadow global a
 
-    interpreter_scope->Declare("a", FromPrim(5));
-    CHECK(interpreter_scope->Get("a") == FromPrim(5));
+    interpreter_scope->Declare("a", (5));
+    CHECK(interpreter_scope->Get("a") == (5));
 
     {
-      Environment<SBObject>::ScopeGuard guard2{&interpreter_scope};
+      Environment<int>::ScopeGuard guard2{&interpreter_scope};
 
       // Assign to a from the first outer scope
 
-      interpreter_scope->TryAssign("a", FromPrim(9));
+      interpreter_scope->TryAssign("a", (9));
     }
 
-    CHECK(interpreter_scope->Get("a") == FromPrim(9));
+    CHECK(interpreter_scope->Get("a") == (9));
   }
 
-  CHECK(interpreter_scope->Get("a") == FromPrim(3));
+  CHECK(interpreter_scope->Get("a") == (3));
 }
 
 //////////////////////////////////////////////////////////////////////
 
 TEST_CASE("Shadowing", "[rt]") {
-  Environment<SBObject> global = Environment<SBObject>::MakeGlobal();
+  Environment<int> global = Environment<int>::MakeGlobal();
 
-  Environment<SBObject>* interpreter_scope = &global;
-  interpreter_scope->Declare("a", FromPrim(3));
+  Environment<int>* interpreter_scope = &global;
+  interpreter_scope->Declare("a", (3));
 
   {
-    Environment<SBObject>::ScopeGuard guard1{&interpreter_scope};
-    CHECK(interpreter_scope->Get("a") == FromPrim(3));
+    Environment<int>::ScopeGuard guard1{&interpreter_scope};
+    CHECK(interpreter_scope->Get("a") == (3));
 
     // Shadow
 
-    interpreter_scope->Declare("a", FromPrim(9));
-    CHECK(interpreter_scope->Get("a") == FromPrim(9));
+    interpreter_scope->Declare("a", (9));
+    CHECK(interpreter_scope->Get("a") == (9));
   }
 
-  CHECK(interpreter_scope->Get("a") == FromPrim(3));
+  CHECK(interpreter_scope->Get("a") == (3));
 }
 
 //////////////////////////////////////////////////////////////////////
 
 TEST_CASE("Assign", "[rt]") {
-  Environment<SBObject> global = Environment<SBObject>::MakeGlobal();
+  Environment<int> global = Environment<int>::MakeGlobal();
 
-  Environment<SBObject>* interpreter_scope = &global;
-  interpreter_scope->Declare("a", FromPrim(3));
+  Environment<int>* interpreter_scope = &global;
+  interpreter_scope->Declare("a", (3));
 
   {
-    Environment<SBObject>::ScopeGuard guard1{&interpreter_scope};
+    Environment<int>::ScopeGuard guard1{&interpreter_scope};
 
     // Insert
-    interpreter_scope->Declare("b", FromPrim(4));
+    interpreter_scope->Declare("b", (4));
 
     // Assign
-    interpreter_scope->TryAssign("a", FromPrim(4));
+    interpreter_scope->TryAssign("a", (4));
   }
 
-  CHECK(interpreter_scope->Get("a") == FromPrim(4));
+  CHECK(interpreter_scope->Get("a") == (4));
 }
 
 //////////////////////////////////////////////////////////////////////
 
 TEST_CASE("Get", "[rt]") {
-  Environment<SBObject> global = Environment<SBObject>::MakeGlobal();
+  Environment<int> global = Environment<int>::MakeGlobal();
 
-  Environment<SBObject>* interpreter_scope = &global;
-  interpreter_scope->Declare("a", FromPrim(3));
+  Environment<int>* interpreter_scope = &global;
+  interpreter_scope->Declare("a", (3));
 
   {
-    Environment<SBObject>::ScopeGuard guard1{&interpreter_scope};
-    interpreter_scope->Declare("b", FromPrim(4));
+    Environment<int>::ScopeGuard guard1{&interpreter_scope};
+    interpreter_scope->Declare("b", (4));
 
     {
-      Environment<SBObject>::ScopeGuard guard2{&interpreter_scope};
-      interpreter_scope->Declare("c", FromPrim(5));
+      Environment<int>::ScopeGuard guard2{&interpreter_scope};
+      interpreter_scope->Declare("c", (5));
       CHECK_NOTHROW(interpreter_scope->Get("a").value());
       CHECK_NOTHROW(interpreter_scope->Get("b").value());
       CHECK_NOTHROW(interpreter_scope->Get("c").value());
